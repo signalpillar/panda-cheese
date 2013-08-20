31aefc33126d043c81091d8f2a40e8b6d5693efb
Vladimir Vitvitskiy <vladimir.vitvitskiy@hp.com>
2013-08-20 13:33:41 +0300

    make new version of lein compile, use aot

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject panda-cheese "0.1.3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :aot [panda-cheese.core]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [enlive "1.1.1"]
                  [net.sourceforge.plantuml/plantuml "7971"]])
